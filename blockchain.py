a7c20dec647d48147a50cf9086c5c6778ee1d455
yolobczuk <wsobczuk2@gmail.com>
2022-12-23 21:58:04 +0100

    Added some functionalities

--- blockchain.py
+++ blockchain.py
@@ -93,6 +93,36 @@ app.config['SECRET_KEY'] = "sosecret"
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
 
+# Add temporary database model
+# to store data about the tickets
+class Tickets(db.Model):
+	id = db.Column(db.Integer, primary_key = True)
+	hash = db.Column(db.String(256), nullable = False)
+	name = db.Column(db.String(40), nullable = False)
+	surname = db.Column(db.String(100), nullable = False)
+	pesel = db.Column(db.String(64), nullable = False)
+	badge = db.Column(db.String(64), nullable = False)
+	amount = db.Column(db.Integer, nullable = False)
+	pen_points = db.Column(db.Integer)
+	date_added = db.Column(db.DateTime, default = datetime.datetime.utcnow)
+
+	def __repr__(self):
+		return '<Name %r>' % self.name
+
+# Add temporary database model
+# to store blockchain data
+class Block(db.Model):
+	hash = db.Column(db.String(256), primary_key = True)
+	proof = db.Column(db.Integer, nullable = False)
+	previous_hash = db.Column(db.String(256), nullable = False)
+	date_added = db.Column(db.DateTime, default = datetime.datetime.utcnow)
+
+	def __repr__(self):
+		return '<Hash %r>' % self.hash
+
+if Block.query.order_by(Block.date_added).count()==0:
+	blockchain.initialise()
+
 # Create main page of the site
 @app.route('/')
 def index():
@@ -150,7 +180,7 @@ def fill_ticket():
 
 		ticket = Tickets.query.filter_by(name = form.name.data).first()
 		if ticket is None:
-			ticket = Tickets(name = form.name.data, surname = form.surname.data, pesel = hashed_pesel,
+			ticket = Tickets(hash = hash, name = form.name.data, surname = form.surname.data, pesel = hashed_pesel,
 							 badge = hashed_badge, amount=form.amount.data, pen_points = form.pen_points.data)
 			db.session.add(ticket)
 			db.session.commit()
@@ -167,31 +197,26 @@ def fill_ticket():
 	blocks = Block.query.order_by(Block.date_added)
 	return render_template('ticket.html', form = form, tickets=tickets, blocks = blocks)
 
-# Add temporary database model
-# to store data about the tickets
-class Tickets(db.Model):
-	id = db.Column(db.Integer, primary_key = True)
-	name = db.Column(db.String(40), nullable = False)
-	surname = db.Column(db.String(100), nullable = False)
-	pesel = db.Column(db.String(64), nullable = False)
-	badge = db.Column(db.String(64), nullable = False)
-	amount = db.Column(db.Integer, nullable = False)
-	pen_points = db.Column(db.Integer)
-	date_added = db.Column(db.DateTime, default = datetime.datetime.utcnow)
+@app.route('/ticket/<hash>', methods = ['GET', 'POST'])
+def get_data(hash):
+	ticket_to_print = Tickets.query.filter_by(hash = hash).first()
+	return render_template("details.html", 
+			ticket_to_print = ticket_to_print)
 
-	def __repr__(self):
-		return '<Name %r>' % self.name
+@app.route('/ticket/details/<hash>', methods = ['GET', 'POST'])
+def get_pesel_data(hash):
+	tickets_to_print = Tickets.query.filter_by(pesel = hash)
+	init_info = tickets_to_print.first()
+	return render_template("details_pesel.html", 
+			ticket_to_print = tickets_to_print, pesel = init_info.pesel, imie = init_info.name, nazwisko = init_info.surname)
 
-# Add temporary database model
-# to store blockchain data
-class Block(db.Model):
-	hash = db.Column(db.String(256), primary_key = True)
-	proof = db.Column(db.Integer, nullable = False)
-	previous_hash = db.Column(db.String(256), nullable = False)
-	date_added = db.Column(db.DateTime, default = datetime.datetime.utcnow)
+@app.route('/ticket/policeman/<hash>', methods = ['GET', 'POST'])
+def get_badge_data(hash):
+	tickets_to_print = Tickets.query.filter_by(badge = hash)
+	init_info = tickets_to_print.first()
+	return render_template("details_badge.html", 
+			ticket_to_print = tickets_to_print, badge = init_info.badge)
 
-	def __repr__(self):
-		return '<Hash %r>' % self.hash
 
 # Mining a new block
 @app.route('/mine_block', methods=['GET'])
@@ -210,15 +235,6 @@ def mine_block():
 
 	return jsonify(response), 200
 
-# Display blockchain in json format
-
-
-@app.route('/get_chain', methods=['GET'])
-def display_chain():
-	response = {'chain': blockchain.chain,
-				'length': len(blockchain.chain)}
-	return jsonify(response), 200
-
 # Check validity of blockchain
 
 
@@ -234,4 +250,4 @@ def valid():
 
 
 # Run the flask server locally
-app.run(host='127.0.0.1', port=8080)
+#app.run(host='127.0.0.1', port=8080)
