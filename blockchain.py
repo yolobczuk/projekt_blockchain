19490029e504153b1f25feda807c691a157b6cdb
yolobczuk <wsobczuk2@gmail.com>
2022-12-26 11:55:34 +0100

    Added login functionality

--- blockchain.py
+++ blockchain.py
@@ -15,11 +15,77 @@ import json
 # Flask is for creating the web
 # app and jsonify is for
 # displaying the blockchain
-from flask import Flask, render_template, flash, jsonify
+from flask import Flask, render_template, flash, jsonify, redirect, url_for
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
-from forms import HashForm, TicketForm
+from forms import HashForm, TicketForm, LoginForm, UserForm
+from flask_login import UserMixin, login_user, LoginManager, login_required, logout_user, current_user
+from werkzeug.security import generate_password_hash, check_password_hash
 import hashlib
+
+# Creating the Web
+# App using flask
+app = Flask(__name__)
+
+app.app_context().push()
+
+# Defining database which
+# simulates the blockchain
+app.config["SQLALCHEMY_DATABASE_URI"] = 'sqlite:///tickets.db'
+app.config['SECRET_KEY'] = "sosecret"
+db = SQLAlchemy(app)
+migrate = Migrate(app, db)
+
+login_manager = LoginManager()
+login_manager.init_app(app)
+login_manager.login_view = 'login'
+
+# Add temporary database model
+# to store data about the tickets
+class Tickets(db.Model):
+	id = db.Column(db.Integer, primary_key = True)
+	hash = db.Column(db.String(256), nullable = False)
+	name = db.Column(db.String(40), nullable = False)
+	surname = db.Column(db.String(100), nullable = False)
+	pesel = db.Column(db.String(64), nullable = False)
+	badge = db.Column(db.String(64), nullable = False)
+	amount = db.Column(db.Integer, nullable = False)
+	pen_points = db.Column(db.Integer)
+	date_added = db.Column(db.DateTime, default = datetime.datetime.utcnow)
+
+	def __repr__(self):
+		return '<Name %r>' % self.name
+
+# Add temporary database model
+# to store blockchain data
+class Block(db.Model):
+	hash = db.Column(db.String(256), primary_key = True)
+	proof = db.Column(db.Integer, nullable = False)
+	previous_hash = db.Column(db.String(256), nullable = False)
+	date_added = db.Column(db.DateTime, default = datetime.datetime.utcnow)
+
+	def __repr__(self):
+		return '<Hash %r>' % self.hash
+
+class Users(db.Model, UserMixin):
+	id = db.Column(db.Integer, primary_key = True)
+	username = db.Column(db.String(20), nullable = False, unique = True)
+	date_added = db.Column(db.DateTime, default = datetime.datetime.utcnow)
+	password_hash = db.Column(db.String(128))
+
+	@property
+	def password(self):
+		raise AttributeError("Password is not a readable attribute!")
+	
+	@password.setter
+	def password(self, password):
+		self.password_hash = generate_password_hash(password)
+
+	def verify_password(self, password):
+		return check_password_hash(self.password_hash, password)
+
+	def __repr__(self):
+		return '<Name %r>' % self.name
 
 class Blockchain:
 	# This function is created
@@ -27,17 +93,20 @@ class Blockchain:
 	# block and set its hash to "0"
 	def __init__(self):
 		self.chain = []
-		self.create_block(proof=1, previous_hash='0', hash = 0, data=['Genesis block'])
+		self.create_block(proof=1, previous_hash='0', hash = 0)
+		blocks = Block.query.order_by(Block.date_added)
+		if blocks.count()>1:
+			for block in blocks:
+				self.create_block(proof=block.proof, previous_hash=block.previous_hash, hash = block.hash)
 
 	# This function is created
 	# to add further blocks
 	# into the chain
-	def create_block(self, proof, previous_hash, hash, data):
+	def create_block(self, proof, previous_hash, hash):
 		block = {'index': len(self.chain) + 1,
 				'timestamp': str(datetime.datetime.now()),
 				'proof': proof,
 				'hash': hash,
-				'data': data,
 				'previous_hash': previous_hash}
 		self.chain.append(block)
 		return block
@@ -73,55 +142,53 @@ class Blockchain:
 
 		return True
 
-# Creating the Web
-# App using flask
-
-
-app = Flask(__name__)
-
-
-app.app_context().push()
-
 # Create the object
 # of the class blockchain
 blockchain = Blockchain()
 
-# Defining database which
-# simulates the blockchain
-app.config["SQLALCHEMY_DATABASE_URI"] = 'sqlite:///tickets.db'
-app.config['SECRET_KEY'] = "sosecret"
-db = SQLAlchemy(app)
-migrate = Migrate(app, db)
+@login_manager.user_loader
+def load_user(user_id):
+	return Users.query.get(int(user_id))
 
-# Add temporary database model
-# to store data about the tickets
-class Tickets(db.Model):
-	id = db.Column(db.Integer, primary_key = True)
-	hash = db.Column(db.String(256), nullable = False)
-	name = db.Column(db.String(40), nullable = False)
-	surname = db.Column(db.String(100), nullable = False)
-	pesel = db.Column(db.String(64), nullable = False)
-	badge = db.Column(db.String(64), nullable = False)
-	amount = db.Column(db.Integer, nullable = False)
-	pen_points = db.Column(db.Integer)
-	date_added = db.Column(db.DateTime, default = datetime.datetime.utcnow)
+@app.route('/login', methods = ['GET', 'POST'])
+def login():
+	form = LoginForm()
+	if form.validate_on_submit():
+		user = Users.query.filter_by(username=form.username.data).first()
+		if user:
+			if check_password_hash(user.password_hash, form.password.data):
+				login_user(user)
+				flash("Logowanie przebiegło pomyślnie")
+				return redirect(url_for('index'))
+			else:
+				flash("Złe hasło, spróbuj ponownie")
+		else:
+			flash("Ten użytkownik nie istnieje, spróbuj ponownie")
+	return render_template('login.html', form = form)
 
-	def __repr__(self):
-		return '<Name %r>' % self.name
+@app.route('/register', methods = ['GET', 'POST'])
+def register():
+	form = UserForm()
 
-# Add temporary database model
-# to store blockchain data
-class Block(db.Model):
-	hash = db.Column(db.String(256), primary_key = True)
-	proof = db.Column(db.Integer, nullable = False)
-	previous_hash = db.Column(db.String(256), nullable = False)
-	date_added = db.Column(db.DateTime, default = datetime.datetime.utcnow)
+	if form.validate_on_submit():
+		user = Users.query.filter_by(username = form.username.data).first()
+		if user is None: 
+			hashed_pw = generate_password_hash(form.password_hash.data, "sha256")
+			user = Users(username = form.username.data, password_hash=hashed_pw)
+			db.session.add(user)
+			db.session.commit()
+		form.username.data = ''
+		form.password_hash.data = ''
 
-	def __repr__(self):
-		return '<Hash %r>' % self.hash
+		flash("Użytkownik zarejestrowany pomyślnie")
+	return render_template('register.html', form = form)   
 
-if Block.query.order_by(Block.date_added).count()==0:
-	blockchain.initialise()
+@app.route('/logout', methods = ['GET', 'POST'])
+@login_required
+def logout():
+	logout_user()
+	flash("Użytkownik wylogowany pomyślnie")
+	return redirect(url_for('login'))
 
 # Create main page of the site
 @app.route('/')
@@ -138,6 +205,7 @@ def page_not_found(e):
 # of printing hash for
 # a given number or string
 @app.route('/hash', methods = ['GET', 'POST'])
+@login_required
 def get_hash():
 	hash = None
 	number = None
@@ -157,6 +225,7 @@ def get_hash():
 # It gives the ability
 # to fill out the ticket data
 @app.route('/ticket', methods = ['GET', 'POST'])
+@login_required
 def fill_ticket():
 	form = TicketForm()
 	hash = None
@@ -173,12 +242,12 @@ def fill_ticket():
 			previous_block = blockchain.get_previous_block()
 			hash, data['proof'] = blockchain.proof_of_work(data)
 			previous_hash = previous_block['hash']
-			blockchain.create_block(proof=data['proof'], hash = hash, previous_hash = previous_hash, data = data)
+			blockchain.create_block(proof=data['proof'], hash = hash, previous_hash = previous_hash)
 			block = Block(hash = hash, proof = data['proof'], previous_hash = previous_hash)
 			db.session.add(block)
 			db.session.commit()
 
-		ticket = Tickets.query.filter_by(name = form.name.data).first()
+		ticket = Tickets.query.filter_by(hash = hash).first()
 		if ticket is None:
 			ticket = Tickets(hash = hash, name = form.name.data, surname = form.surname.data, pesel = hashed_pesel,
 							 badge = hashed_badge, amount=form.amount.data, pen_points = form.pen_points.data)
@@ -198,24 +267,40 @@ def fill_ticket():
 	return render_template('ticket.html', form = form, tickets=tickets, blocks = blocks)
 
 @app.route('/ticket/<hash>', methods = ['GET', 'POST'])
+@login_required
 def get_data(hash):
 	ticket_to_print = Tickets.query.filter_by(hash = hash).first()
 	return render_template("details.html", 
-			ticket_to_print = ticket_to_print)
+			ticket_to_print = ticket_to_print, hash = hash)
 
 @app.route('/ticket/details/<hash>', methods = ['GET', 'POST'])
+@login_required
 def get_pesel_data(hash):
-	tickets_to_print = Tickets.query.filter_by(pesel = hash)
-	init_info = tickets_to_print.first()
-	return render_template("details_pesel.html", 
-			ticket_to_print = tickets_to_print, pesel = init_info.pesel, imie = init_info.name, nazwisko = init_info.surname)
+	tickets_to_print = Tickets.query.filter_by(pesel = hash).order_by(Tickets.date_added)
+	init_info = Tickets.query.filter_by(pesel = hash).first()
+	if tickets_to_print.count() > 0:
+		return render_template("details_pesel.html", 
+				tickets = tickets_to_print, pesel = init_info.pesel, imie = init_info.name, nazwisko = init_info.surname)
+	else:
+		return render_template("details_pesel.html", hash = hash)
 
 @app.route('/ticket/policeman/<hash>', methods = ['GET', 'POST'])
+@login_required
 def get_badge_data(hash):
-	tickets_to_print = Tickets.query.filter_by(badge = hash)
-	init_info = tickets_to_print.first()
-	return render_template("details_badge.html", 
-			ticket_to_print = tickets_to_print, badge = init_info.badge)
+	tickets_to_print = Tickets.query.filter_by(badge = hash).order_by(Tickets.date_added)
+	init_info = Tickets.query.filter_by(badge = hash).first()
+	if tickets_to_print.count() > 0:
+		return render_template("details_badge.html", 
+				tickets = tickets_to_print, badge = init_info.badge)
+	else:
+		return render_template("details_badge.html", hash = hash)
+
+@app.route('/ticket/all', methods = ['GET', 'POST'])
+@login_required
+def get_all_tickets():
+	blocks = Block.query.order_by(Block.date_added)
+	return render_template("all_tickets.html", 
+			blocks = blocks)
 
 
 # Mining a new block
--- forms.py
+++ forms.py
@@ -1,6 +1,6 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, SubmitField, IntegerField
-from wtforms.validators import DataRequired
+from wtforms import StringField, SubmitField, IntegerField, PasswordField
+from wtforms.validators import DataRequired, EqualTo
 
 class HashForm(FlaskForm):
 	number = StringField("Podaj ciąg znaków do hashowania", validators=[DataRequired()])
@@ -14,3 +14,14 @@ class TicketForm(FlaskForm):
 	amount = IntegerField("Podaj wartość mandatu", validators=[DataRequired()])
 	pen_points = IntegerField("Podaj liczbę przyznanych punktów karnych", validators=[])
 	submit = SubmitField("Zapisz mandat")
+
+class LoginForm(FlaskForm):
+	username = StringField("Podaj identyfikator użytkownika", validators=[DataRequired()])
+	password= PasswordField("Podaj hasło", validators=[DataRequired()])
+	submit = SubmitField("Zaloguj")
+
+class UserForm(FlaskForm):
+	username = StringField("Podaj identyfikator", validators=[DataRequired()])
+	password_hash = PasswordField("Podaj hasło", validators=[DataRequired(), EqualTo('password_hash2', message='Hasła muszą być identyczne')])
+	password_hash2 = PasswordField("Podaj hasło ponownie", validators=[DataRequired()])
+	submit = SubmitField("Zarejestruj")
